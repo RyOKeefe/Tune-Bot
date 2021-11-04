2406022919c23eb0341a9fcf6ed9f62119a932af
Ryan O'Keefe <rokeefe@ufl.edu>
2021-11-04 18:43:33 -0400

    Update server_config.py

--- server_config.py
+++ server_config.py
@@ -5,6 +5,7 @@ from route_config import *
 app.debug = True
 host = os.environ.get('IP', '0.0.0.0')
 port = int(os.environ.get('PORT', 55677))
-app.run(host=host, port=port)
+if __name__ == '__main__':
+    app.run(host=host, port=port)
 
 
