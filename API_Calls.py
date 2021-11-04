e29a29fb067d1cd545d3b5d962bff09a62febaeb
Ryan O'Keefe <rokeefe@ufl.edu>
2021-11-04 17:01:22 -0400

    added recommendations. Added backend

--- API_Calls.py
+++ API_Calls.py
@@ -5,29 +5,35 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from secret_key import client_ID, client_secret
 
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_ID, client_secret))
+
 
 # returns a dict list of up to size {limit} containing the search results
 def get_album(album_name, limit=1):
-    spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_ID, client_secret))
+    album_name = bytes(album_name, "utf-8").decode("unicode_escape")
     return spotify.search(album_name, type='album', limit=limit)['albums']['items']
 
 
 # returns a dict list of up to size {limit} containing the search results
 def get_artist(artist_name, limit=1):
-    spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_ID, client_secret))
+    artist_name = bytes(artist_name, "utf-8").decode("unicode_escape")
     return spotify.search(artist_name, type='artist', limit=limit)['artists']['items']
 
 
 # returns a dict list of up to size {limit} containing the search results
 def get_song(song_name, limit=1):
-    spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_ID, client_secret))
+    song_name = bytes(song_name, "utf-8").decode("unicode_escape")
     return spotify.search(song_name, type='track', limit=limit)['tracks']['items']
 
 
 # returns a dict list of up to size {limit} containing the search results
 def get_playlist(playlist_name, limit=1):
-    spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_ID, client_secret))
+    playlist_name = bytes(playlist_name, "utf-8").decode("unicode_escape")
     return spotify.search(playlist_name, type='playlist', limit=limit)['playlists']['items']
+
+
+def get_recommendations(genres=[], tracks=[], artists=[], limit=1):
+    return spotify.recommendations(seed_genres=genres, seed_artists=artists, seed_tracks=tracks, limit=limit)
 
 
 def main():
---
+++ route_config.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+from API_Calls import *
+
+# app reference
+
+app = Flask(__name__)
+
+
+@app.before_request
+def before_request():
+    print('request received')
+
+
+@app.route('/api/recommendation')
+def recommendations_default():
+    return "Welcome to tunebot backend. Make a request."
+
+
+@app.route('/api/recommendation', methods=['POST'])
+def recommendation():
+    genre_list = []
+    track_list = []
+    artist_list = []
+
+    for genre in request.json["queryResult"]["parameters"]["music-genre"]:
+        genre_list.append(genre)
+
+    for artist in request.json["queryResult"]["parameters"]["music-artist"]:
+        artist_list.append(get_artist(artist, limit=1)[0]['id'])
+
+    for track in request.json["queryResult"]["parameters"]["song-name"]:
+        track_list.append(get_song(track, limit=1)[0]['id'])
+    temp = get_recommendations(genres=genre_list, artists=artist_list, tracks=track_list)['tracks'][0]
+    return temp
