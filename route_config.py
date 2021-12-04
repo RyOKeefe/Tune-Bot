0d892231c5d54babaee3aebebc9688e579cff4b7
Ryan O'Keefe <rokeefe@ufl.edu>
2021-12-04 13:40:51 -0500

    Update route_config.py

--- route_config.py
+++ route_config.py
@@ -43,61 +43,6 @@ def recommendation():
             return artist_recommendation(req_data)
 
 
-def iterate(req_data):
-    genre_list = []
-    track_list = []
-    artist_list = []
-    response = None
-    try:
-
-        for genre in req_data["queryResult"]["parameters"]["music-genre"]:
-            genre_list.append(genre)
-
-        for artist in req_data["queryResult"]["parameters"]["music-artist"]:
-            artist_list.append(get_artist(artist, limit=1)[0]['id'])
-
-        for track in req_data["queryResult"]["parameters"]["song-name"]:
-            track_list.append(get_song(track, limit=1)[0]['id'])
-        print("Successful Recommendation")
-        response = get_recommendations(genres=genre_list, artists=artist_list, tracks=track_list)
-        prev_recommendation['prev_song'] = response['tracks'][0]['name']
-        prev_recommendation['prev_artist'] = response['tracks'][0]['artists'][0]['name']
-
-    except TypeError as error:
-        print("Invalid Genre Input")
-        print(error)
-        response = get_recommendations(genres=[], artists=artist_list, tracks=track_list)
-    except Exception as error:
-        print("Total failure")
-        print(error)
-        response = get_recommendations(genres=[], artists=[get_artist("Katy Perry", limit=1)[0]['id']],
-                                       tracks=[get_song("California Girls", limit=1)[0]['id']])
-
-    if len(req_data["queryResult"]["parameters"]["song-name"]) > 0:
-        print("Track Input:" + req_data["queryResult"]["parameters"]["song-name"][0])
-    if len(req_data["queryResult"]["parameters"]["music-genre"]) > 0:
-        print("Genre Input:" + req_data["queryResult"]["parameters"]["music-genre"][0])
-    if len(req_data["queryResult"]["parameters"]["music-artist"]) > 0:
-        print("Artist Input:" + req_data["queryResult"]["parameters"]["music-artist"][0])
-
-
-    print("Track recommendation:" + response['tracks'][0]['name'])
-    print("Artist recommendation:" + response['tracks'][0]['artists'][0]['name'])
-
-    return {
-        "fulfillmentMessages": [
-            {
-                "text": {
-                    "text": [
-                        "You should try listening to " + response['tracks'][0]['name'] + " by " +
-                        response['tracks'][0]['artists'][0]['name'] + ". Would you like another music recommendation?"
-                    ]
-                }
-            }
-        ]
-    }
-
-
 def base_recommendation(req_data):
     genre_list = []
     track_list = []
@@ -121,7 +66,7 @@ def base_recommendation(req_data):
     except Exception as error:
         print("Total failure")
         print(error)
-        eturn
+        return
         {
             "fulfillmentMessages": [
                 {
